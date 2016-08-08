0870e30b6d694f6a9eb2ce15b79a8ad94fa05bec
Shafique Jamal <shafique.jamal@gmail.com>
2016-08-08 19:20:50 -0400

    Add some more code for use in another project that adds this one as a dependency

--- src/main/scala/com/eigenroute/testmavenrepo/Hello.scala
+++ src/main/scala/com/eigenroute/testmavenrepo/Hello.scala
@@ -1,7 +1,11 @@
 package com.eigenroute.testmavenrepo
 
+import SomeTestConfigParams._
+
 object Hello {
   def main(args: Array[String]): Unit = {
     println("Hello from com.eigenroute.testmavenrepo - 0.1-SNAPSHOT")
+    println(s"1. $configParam1")
+    println(s"2. $configParam2")
   }
 }
---
+++ src/main/scala/com/eigenroute/testmavenrepo/SomeTestConfigParams.scala
@@ -0,0 +1,8 @@
+package com.eigenroute.testmavenrepo
+
+object SomeTestConfigParams {
+
+  val configParam1 = "configuration parameter one"
+  val configParam2 = "configuration parameter two"
+
+}
